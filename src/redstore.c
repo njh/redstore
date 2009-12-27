8ec1b6665831a228578c455683b8179419e32c35
Nicholas J Humfrey <njh@aelius.com>
2009-12-27 17:49:25 +0000

    Implemented basic logging, usage, option parsing and signal handling.
    git-svn-id: https://redstore.googlecode.com/svn/trunk@5 853f923e-f49a-11de-bb94-7564724c0657

--- src/redstore.c
+++ src/redstore.c
@@ -1,18 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <signal.h>
+#include <time.h>
 #include <unistd.h>
+#include <ctype.h>
 
-#include "config.h"
 #include "redstore.h"
 
 
-#define PORT 8080
+// ------- Globals -------
+int quiet = 0;					// Only display error messages
+int verbose = 0;				// Increase number of logging messages
+int running = 1;        // True while still running
 
-int keep_running = 1;
+
+static void termination_handler (int signum)
+{
+    switch(signum) {
+        case SIGHUP:    redstore_info("Got hangup signal."); break;
+        case SIGTERM:   redstore_info("Got termination signal."); break;
+        case SIGINT:    redstore_info("Got interupt signal."); break;
+    }
+    
+    signal(signum, termination_handler);
+    
+    // Signal the main thead to stop
+    running = 0;
+}
 
 
-int dispatcher (void *cls, struct MHD_Connection *connection,
+void redstore_log( RedstoreLogLevel level, const char* fmt, ... )
+{
+    time_t t=time(NULL);
+    char time_str[32];
+    va_list args;
+    
+    
+    // Display the message level
+    if (level == REDSTORE_DEBUG ) {
+        if (!verbose) return;
+        printf( "[DEBUG]  " );
+    } else if (level == REDSTORE_INFO ) {
+        if (quiet) return;
+        printf( "[INFO]   " );
+    } else if (level == REDSTORE_ERROR ) {
+        printf( "[ERROR]  " );
+    } else if (level == REDSTORE_FATAL ) {
+        printf( "[FATAL]  " );
+    } else {
+        printf( "[UNKNOWN]" );
+    }
+
+    // Display timestamp
+    ctime_r( &t, time_str );
+    time_str[strlen(time_str)-1]=0; // remove \n
+    printf( "%s  ", time_str );
+    
+    // Display the error message
+    va_start( args, fmt );
+    vprintf( fmt, args );
+    printf( "\n" );
+    va_end( args );
+    
+    // If fatal then stop
+    if (level == REDSTORE_FATAL) {
+        if (running) {
+            // Quit gracefully
+            running = 0;
+        } else {
+            printf( "Fatal error while quiting; exiting immediately." );
+            exit(-1);
+        }
+    }
+}
+
+static int dispatcher (void *cls, struct MHD_Connection *connection,
                       const char *url, const char *method,
                       const char *version, const char *upload_data,
                       size_t *upload_data_size, void **con_cls)
@@ -27,7 +90,7 @@ int dispatcher (void *cls, struct MHD_Connection *connection,
     request.upload_data_size = upload_data_size;
     request.con_cls = con_cls;
     
-    printf("%s: %s\n", method, url);
+    redstore_debug("%s: %s", method, url);
 
     if (strcmp(request.url, "/")==0) {
         return handle_homepage(&request);
@@ -38,21 +101,63 @@ int dispatcher (void *cls, struct MHD_Connection *connection,
     }
 }
 
-int main()
+// Display how to use this program
+static void usage()
 {
-    struct MHD_Daemon *daemon;
+    printf("%s version %s\n\n", PACKAGE_NAME, PACKAGE_VERSION);
+    printf("Usage: %s [options]\n", PACKAGE_TARNAME);
+    printf("   -p <port>     Port number to run HTTP server on (default %d)\n", DEFAULT_PORT);
+    printf("   -v            Enable verbose mode\n");
+    printf("   -q            Enable quiet mode\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    struct MHD_Daemon *daemon = NULL;
+    int port = DEFAULT_PORT;
+    int opt = -1;
     
-    daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
-                   &dispatcher, NULL, MHD_OPTION_END);
-    if (daemon == NULL) {
-        fprintf(stderr, "Failed to start daemon.\n");
-        return 1;
+    // Make STDOUT unbuffered - we use it for logging
+    setbuf(stdout, NULL);
+
+    // Parse Switches
+    while ((opt = getopt(argc, argv, "p:vqh")) != -1) {
+        switch (opt) {
+            case 'p':  port = atoi(optarg); break;
+            case 'v':  verbose = 1; break;
+            case 'q':  quiet = 1; break;
+            default:  usage(); break;
+        }
     }
     
-    while (keep_running) {
+    // Validate parameters
+    if (quiet && verbose) {
+        redstore_error("Can't be quiet and verbose at the same time.");
+        usage();
+    }
+
+    // Setup signal handlers
+    signal(SIGTERM, termination_handler);
+    signal(SIGINT, termination_handler);
+    signal(SIGHUP, termination_handler);
+
+
+    redstore_info("Starting HTTP server on port %d", port);
+    daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, port,
+                   NULL, NULL,
+                   &dispatcher, NULL,
+                   MHD_OPTION_END
+    );
+    if (daemon == NULL) {
+        redstore_fatal("Failed to start HTTP daemon: \n");
+    }
+    
+    while (running) {
         sleep(1);
     }
     
-    MHD_stop_daemon (daemon);
-    return 0;
+    MHD_stop_daemon(daemon);
+ 
+    return 0;   // FIXME: should return non-zero if there was a fatal error
 }
--- src/redstore.h
+++ src/redstore.h
@@ -1,3 +1,4 @@
+#include "config.h"
 
 #include <stdarg.h>
 #include <stdint.h>
@@ -8,8 +9,40 @@
 #include <microhttpd.h>
 
 
-#define DEFAULT_PORT 8080
+#ifndef _REDSTORE_H_
+#define _REDSTORE_H_
 
+
+// ------- Constants -------
+#define DEFAULT_PORT    (8080)
+
+
+
+
+// ------- Logging ---------
+typedef enum {
+    REDSTORE_DEBUG=1,   // Only display debug if verbose
+    REDSTORE_INFO,      // Don't show info when quiet
+    REDSTORE_ERROR,     // Always display
+    REDSTORE_FATAL      // Quit if fatal
+} RedstoreLogLevel;
+
+
+#define redstore_debug( ... ) \
+		redstore_log(REDSTORE_DEBUG, __VA_ARGS__ )
+
+#define redstore_info( ... ) \
+		redstore_log(REDSTORE_INFO, __VA_ARGS__ )
+
+#define redstore_error( ... ) \
+		redstore_log( REDSTORE_ERROR, __VA_ARGS__ )
+
+#define redstore_fatal( ... ) \
+		redstore_log( REDSTORE_FATAL, __VA_ARGS__ )
+
+
+
+// ------- Structures -------
 
 typedef struct http_request {
     void *cls;
@@ -26,6 +59,11 @@ typedef struct http_request {
 typedef struct MHD_Response http_response_t;
 
 
+// ------- Globals ---------
+
+
+
+// ------- Prototypes -------
 
 int handle_homepage(http_request_t *request);
 int handle_favicon(http_request_t *request);
@@ -35,3 +73,8 @@ int handle_html_page(http_request_t *request, unsigned int status,
 int handle_static_data(http_request_t *request, unsigned int status,
                        void* content, size_t content_size,
                        const char* content_type);
+
+void redstore_log( RedstoreLogLevel level, const char* fmt, ... );
+
+
+#endif
