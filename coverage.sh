4ca881f355a299e930f7fad68d32a7bbdc9535f9
Nicholas J Humfrey <njh@aelius.com>
2010-01-31 13:20:10 +0000

    Created additional http_request test suite. Wrote script to generage a test coverage report.
    git-svn-id: https://redstore.googlecode.com/svn/trunk@79 853f923e-f49a-11de-bb94-7564724c0657

---
+++ coverage.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Create an HTML Coverage Report using lcov
+#
+
+if ! which lcov &>/dev/null; then
+    echo "lcov is not installed."
+    exit -1
+fi
+
+if ! which genhtml &>/dev/null; then
+    echo "genhtml is not installed."
+    exit -1
+fi
+
+
+cd `dirname $0`
+
+if [ ! -d coverage ]; then
+    mkdir -f coverage
+fi
+
+if [ -e Makefile ]; then
+    make clean
+fi
+
+find . -name '*.gcda' -o -name '*.gcno' -o -name '*.gcov' -delete
+lcov --directory src --zerocounters
+
+if ! ./configure --enable-debug CFLAGS='-fprofile-arcs -ftest-coverage'; then
+    echo "Failed to configure project; aborting."
+    exit -1
+fi
+
+if ! make check; then
+    echo "Some tests failed; aborting."
+    exit -1
+fi
+
+lcov --directory src --capture --output-file coverage.info
+genhtml -t "RedStore Coverage Report" --output-directory coverage coverage.info
