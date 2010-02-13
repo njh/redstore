d80d875933bc19c8d5f7d8e552b4111c7f66829a
Nicholas J Humfrey <njh@aelius.com>
2010-02-13 23:18:55 +0000

    Added tests to ensure that server responses are valid XML
    git-svn-id: https://redstore.googlecode.com/svn/trunk@143 853f923e-f49a-11de-bb94-7564724c0657

--- tests/integration_test.pl
+++ tests/integration_test.pl
@@ -4,18 +4,18 @@ use POSIX ":sys_wait_h";
 use File::Basename qw(dirname);
 use LWP::UserAgent;
 use HTTP::Response;
-use XML::Parser;
 use Errno;
-use URI::Escape;
 use warnings;
 use strict;
 
-use Test::More tests => 20;
+use Test::More tests => 35;
 
 my $REDSTORE = dirname(__FILE__) . '/../src/redstore';
 my $TEST_CASE_URI = 'http://www.w3.org/2000/10/rdf-tests/rdfcore/xmlbase/test001.rdf';
-my ($request, $response);
+my $ESCAPED_TEST_CASE_URI = 'http%3A%2F%2Fwww.w3.org%2F2000%2F10%2Frdf-tests%2Frdfcore%2Fxmlbase%2Ftest001.rdf';
 
+# Create a libwww-perl user agent
+my ($request, $response);
 my $ua = LWP::UserAgent->new;
 $ua->timeout(10);
 $ua->max_redirect(0);
@@ -33,27 +33,27 @@ is_running($pid);
 # Test getting the homepage
 $response = $ua->get($base_url);
 is($response->code, 200, "Getting homepage is successful");
-# FIXME: validate the XML
+is_wellformed($response->content, "Homepage is valid XML");
 
 # Test getting the query page
 $response = $ua->get($base_url.'query');
 is($response->code, 200, "Getting query page is successful");
-# FIXME: validate the XML
+is_wellformed($response->content, "Query page is valid XML");
 
 # Test getting information page
 $response = $ua->get($base_url.'info');
 is($response->code, 200, "Getting info page is successful");
-# FIXME: validate the XML
+is_wellformed($response->content, "Info page is valid XML");
 
 # Test getting formats page
 $response = $ua->get($base_url.'formats');
 is($response->code, 200, "Getting formats page is successful");
-# FIXME: validate the XML
+is_wellformed($response->content, "Formats page is valid XML");
 
 # Test getting load page
 $response = $ua->get($base_url.'load');
 is($response->code, 200, "Getting load page is successful");
-# FIXME: validate the XML
+is_wellformed($response->content, "Load page is valid XML");
 
 # Test getting the favicon
 $response = $ua->get($base_url.'favicon.ico');
@@ -61,38 +61,53 @@ is($response->code, 200, "Getting favicon.ico is successful");
 is($response->content_type, 'image/x-icon', "Getting favicon.ico set the content type");
 is($response->content_length, 318, "Getting favicon.ico set the content length header");
 
+# Test getting empty list of graphs
+$response = $ua->get($base_url.'data');
+is($response->code, 200, "Getting empty list of graphs is successful");
+like($response->content, qr[<ul>\n</ul>], "List of graphs page contains empty unordered list");
+is_wellformed($response->content, "Empty list of graphs is valid XML");
+
 # Test PUTing a graph
-$request = HTTP::Request->new( 'PUT', $base_url.'data/'.uri_escape($TEST_CASE_URI) );
+$request = HTTP::Request->new( 'PUT', $base_url.'data/'.$ESCAPED_TEST_CASE_URI );
 $request->content( read_fixture('test001.rdf') );
 $request->content_length( length($request->content) );
 $request->content_type( 'application/rdf+xml' );
 $response = $ua->request($request);
 is($response->code, 200, "Putting a data into a graph");
 like($response->content, qr/Added 1 triples to graph/, "Load response message contain triple count");
+is_wellformed($response->content, "Response to putting a graph is valid XML");
 
 # Test getting a graph
-$request = HTTP::Request->new( 'GET', $base_url.'data/'.uri_escape($TEST_CASE_URI) );
+$request = HTTP::Request->new( 'GET', $base_url.'data/'.$ESCAPED_TEST_CASE_URI );
 $request->header('Accept', 'application/x-ntriples');
 $response = $ua->request($request);
 is($response->code, 200, "Getting a graph is successful");
 is($response->content_type, "application/x-ntriples", "Content Type data is correct");
 is($response->content, "<http://example.org/dir/file#frag> <http://example.org/value> \"v\" .\n", "Graph data is correct");
 
+# Test getting list of graphs
+$response = $ua->get($base_url.'data');
+is($response->code, 200, "Getting list of graphs is successful");
+like($response->content, qr[<li><a href="/data/$ESCAPED_TEST_CASE_URI">$TEST_CASE_URI</a></li>], "List of graphs page contains graph that was added");
+is_wellformed($response->content, "Graph list is valid XML");
+
 # Test getting a non-existant graph
 $response = $ua->get($base_url."data/http%3A%2F%2Fwww.example.com%2Finvalid");
 is($response->code, 404, "Getting a non-existant graph returns 404");
+is_wellformed($response->content, "Graph not found page is valid XML");
 
 # Test removing trailing slash
 $response = $ua->get($base_url."query/");
 is($response->code, 301, "Getting a URL with a trailing slash returns 301");
 is($response->header('Location'), "/query", "Getting a URL with a trailing slash returns location without trailing slash");
+is_wellformed($response->content, "Redirect page is valid XML");
 
 # Test a SELECT query
 $response = $ua->get($base_url."sparql?query=SELECT+*+WHERE+%7B%3Fs+%3Fp+%3Fo%7D%0D%0A&format=xml");
 is($response->code, 200, "SPARQL SELECT query is successful");
 is($response->content_type, "application/sparql-results+xml", "SPARQL SELECT query Content Type data is correct");
 like($response->content, qr[<binding name="o"><literal>v</literal></binding>], "SPARQL SELECT Query contains right content");
-# FIXME: validate the XML
+is_wellformed($response->content, "SPARQL response is valid XML");
 
 END {
     stop_redstore($pid);
@@ -161,3 +176,23 @@ sub read_fixture {
     close(FILE);
     return $data;
 }
+
+sub is_wellformed {
+    my ($xml, $name) = @_;
+
+	SKIP: {
+		eval { require XML::Parser; };
+		skip("XML::Parser not installed", 1) if $@;
+
+		my $parser = new XML::Parser(ErrorContext => 2);
+    	eval { $parser->parse($xml); };
+    	if ($@) {
+        	$@ =~ s/at \/.*?$//s;
+        	print STDERR "$@\n";
+        	fail($name);
+    	} else {
+        	pass($name);
+    	}
+    }
+}
+
