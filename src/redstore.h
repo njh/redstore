1a5ff6a8744f4af7600baa9f9358104f99ecd5cc
Nicholas J Humfrey <njh@aelius.com>
2010-03-31 11:14:49 +0000

    Fixes for Raptor v1
    git-svn-id: https://redstore.googlecode.com/svn/trunk@174 853f923e-f49a-11de-bb94-7564724c0657

--- src/redstore.h
+++ src/redstore.h
@@ -143,4 +143,19 @@ void redstore_log(RedstoreLogLevel level, const char *fmt, ...);
 
 
 
+
+#ifndef RAPTOR_V2_AVAILABLE
+
+#define raptor_new_iostream_to_file_handle(world, fh) raptor_new_iostream_to_file_handle(fh)
+#define raptor_new_iostream_to_string(world, string_p, len_p, malloc) raptor_new_iostream_to_string(string_p, len_p, malloc)
+#define raptor_iostream_write_bytes(ptr, size, nmemb, iostr) raptor_iostream_write_bytes(iostr, ptr, size, nmemb)
+#define raptor_iostream_write_byte(byte, iostr) raptor_iostream_write_byte(iostr, byte)
+#define raptor_iostream_counted_string_write(string, len, iostr) raptor_iostream_write_counted_string(iostr, string, len)
+#define raptor_iostream_string_write(string, iostr) raptor_iostream_write_string(iostr, string)
+
+#define raptor_string_ntriples_write(str, len, delim, iostr) raptor_iostream_write_string_ntriples(iostr, str, len, delim)
+
+#endif /* !RAPTOR_V2_AVAILABLE */
+
+
 #endif
