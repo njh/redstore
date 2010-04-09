d88468da594cb81829c0cc51d1badbcab67fcc0b
Nicholas J Humfrey <njh@aelius.com>
2010-04-09 19:34:35 +0000

    Changed static build to using a snapshot of rasqal 0.9.20 (rasqal-20100409)
    git-svn-id: https://redstore.googlecode.com/svn/trunk@218 853f923e-f49a-11de-bb94-7564724c0657

--- build-scripts/build-static.pl
+++ build-scripts/build-static.pl
@@ -19,7 +19,7 @@ my $packages = [
     },
     {
         'dirname' => 'check-0.9.8',
-        'url' => 'http://www.aelius.com/njh/redstore/check-20100409.tar.gz',
+        'url' => 'http://redstore.googlecode.com/files/check-20100409.tar.gz',
         'checkfor' => 'bin/checkmk',
     },
     {
@@ -79,7 +79,8 @@ my $packages = [
         'checkfor' => 'lib/pkgconfig/raptor.pc',
     },
     {
-        'url' => 'http://download.librdf.org/source/rasqal-0.9.16.tar.gz',
+        'dirname' => 'rasqal-0.9.20',
+        'url' => 'http://redstore.googlecode.com/files/rasqal-20100409.tar.gz',
         'config' => "./configure $DEFAULT_CONFIGURE_ARGS '--enable-query-languages=sparql rdql laqrs'",
         'checkfor' => 'lib/pkgconfig/rasqal.pc',
         
